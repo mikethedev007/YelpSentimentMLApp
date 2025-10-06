b4767121401deb3efb1baedfa49942462fc4fa9e
mikethedev007 <softwaredevmike2020@gmail.com>
2025-10-06 05:36:42 +0100

    Add project files.

---
+++ myMLApp/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using MyMLApp;
+
+//add input data
+var sampleData = new SentimentModel.ModelInput()
+{
+    Col0 = "This Restaurant was wonderful."
+};
+
+//Load model and Predict output of sample data
+var result = SentimentModel.Predict(sampleData);
+
+//If the Prediction is 1 then Positive; otherwise Prediction is Negative
+var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
